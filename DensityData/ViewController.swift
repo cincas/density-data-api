9a8d43e7fc28545b2e87c0c5a21198d60407883d
Tyler Yang <tyang@ingogo.mobi>
2019-04-11 20:20:26 +1000

    Initial Commit

--- DensityData/ViewController.swift
+++ DensityData/ViewController.swift
@@ -1,20 +1,9 @@
-//
-//  ViewController.swift
-//  DensityData
-//
-//  Created by Tyler Yang on 11/4/19.
 //  Copyright © 2019 cincas. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
-
-
 }
-
--- DensityDataTests/DensityDataTests.swift
+++ DensityDataTests/DensityDataTests.swift
@@ -1,34 +1,13 @@
-//
-//  DensityDataTests.swift
-//  DensityDataTests
-//
-//  Created by Tyler Yang on 11/4/19.
 //  Copyright © 2019 cincas. All rights reserved.
-//
 
 import XCTest
 @testable import DensityData
 
 class DensityDataTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  override func setUp() {
+  }
+  
+  override func tearDown() {
+  }
 }
+
